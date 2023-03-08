1a46af3e9c4dcda51aa6ef8078c5d3605f1d265e
unkn0wn-root <unkn0wn_root@tuta.io>
2023-03-08 21:37:39 +0100

    [bodybuilder] fix site utl formatting

--- src/controllers/bodyBuildier.ts
+++ src/controllers/bodyBuildier.ts
@@ -9,7 +9,7 @@ const getBody = (confession: IConfession, user: IUser) => {
 	return process.env.NODE_ENV === 'development' ?
 		`${confession.text}\n\n \n${confession._id}\n\n \n#HejtoAPITesty \n`
 		:
-		`#anonimowehejtowyznania\n\n \n\n${confession.text}\n\n \n\n\\---\n\n[Kliknij tutaj, aby odpowiedzieć w tym wątku anonimowo](${config.siteURL}/reply/${confession._id}) \n[Kliknij tutaj, aby wysłać OPowi anonimową wiadomość prywatną](${config.siteURL}/conversation/${confession._id}/new) \nID: #${confession._id}\nPost dodany za pomocą AnonimoweHejtoWyznania (${config.siteURL}) - Zaakceptowane przez: [${user.username}](${config.siteURL}/conversation/U_${user.username}/new)`
+		`#anonimowehejtowyznania\n\n \n\n${confession.text}\n\n \n\n\\---\n\n[Kliknij tutaj, aby odpowiedzieć w tym wątku anonimowo](${config.siteURL}/reply/${confession._id}) \n[Kliknij tutaj, aby wysłać OPowi anonimową wiadomość prywatną](${config.siteURL}/conversation/${confession._id}/new) \nID: #${confession._id}\nPost dodany za pomocą AnonimoweHejtoWyznania: ${config.siteURL} - Zaakceptowane przez: [${user.username}](${config.siteURL}/conversation/U_${user.username}/new)`
 }
 
 async function getEntryBody(confession, user) {
